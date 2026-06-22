078590aaa35d291de2308e08e1227a9b7409b219
Global Sports <utproducts1@gmail.com>
2026-06-22 12:31:47 +0000

    Add jsVectorMap type declaration for build

---
+++ types/jsvectormap.d.ts
@@ -0,0 +1,6 @@
+declare module "jsvectormap" {
+  const JsVectorMap: any;
+  export default JsVectorMap;
+}
+declare module "jsvectormap/dist/maps/world.js";
+declare module "jsvectormap/dist/jsvectormap.min.css";
